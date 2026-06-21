aee4c9b51acf690a71fe68ccceb1ed55e95bded6
dysfne <dysfne@gmail.com>
2026-06-20 22:06:17 -0700

    add functionality for custom-defined codes

--- src/ntstts_decode.c
+++ src/ntstts_decode.c
@@ -1,4 +1,5 @@
 /* filename: ntstts_decode.c */
+#include <assert.h>
 
 #include "ntstts.h"
 
@@ -11,6 +12,8 @@ extract_field(ULONG status, const struct ntstatus_subfield_desc *desc)
 void
 decoded_ntstatus(ULONG status, struct ntstatus_decoded *out)
 {
+    assert(out != NULL && "ntstatus_decoded: out must not be NULL");
+
     out->raw = status;
 
     out->severity_value = 
@@ -28,11 +31,27 @@ decoded_ntstatus(ULONG status, struct ntstatus_decoded *out)
     out->code_value = 
         extract_field(out->raw, &ntstatus_subfield_table[NTSTATUS_FIELD_CODE]);
 
+    w_lookup_severity(out->severity_value, &out->severity_info);
+
     /* call these functions only if the NTSTATUS is Microsoft defined */
-    if (out->customer_value == 0) {
-        w_lookup_severity(out->severity_value, &out->severity_info);
+    if (!out->customer_value) {
         w_lookup_facility(out->facility_value, &out->facility_info);
         w_lookup_ntstatus(out->raw, &out->status_info);
+    } 
+    /* if the NTSTATUS is customer-defined run this */
+    else if (out-customer_value) {
+        out->facility_info = (struct ntstatus_facility_entry) {
+            .name = "CUSTOMER_DEFINED_FACILITY",
+            .value = out->facility_value
+        };
+
+        out->status_info = (struct ntstatus_entry) {
+            .name = "CUSTOMER_DEFINED_STATUS",
+            .value = out->raw,
+            .description = 
+                "Customer-defined NTSTATUS value. The meaning of this status code "
+                "is private to the component, driver, or vendor that defined it."
+        
+        };
     }
-    /* add functionality for custom ntstatus codes */
 }
