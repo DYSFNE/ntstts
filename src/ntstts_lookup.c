93032c2bdd6ca7cd7cfa0d412f194b1c3eed7167
dysfne <dysfne@gmail.com>
2026-06-21 16:21:59 -0400

    Update

--- src/ntstts_lookup.c
+++ src/ntstts_lookup.c
@@ -2,10 +2,10 @@
 /* this file can be reduced to one function */
 #include "ntstts.h"
 
-const struct *ntstts_severity_entry
+const struct ntstts_severity_entry *
 ntstts_lookup_severity(uint32_t severity)
 {
-    size_t = i;
+    size_t i;
 
     for (i = 0; i < severity_table_count; i++) {
         if (ntstts_severity_table[i].value == severity)
@@ -15,10 +15,10 @@ ntstts_lookup_severity(uint32_t severity)
     return NULL;
 }
 
-const struct *ntstts_facility_entry
+const struct ntstts_facility_entry *
 ntstts_lookup_facility(uint32_t facility)
 {
-    size_t = i;
+    size_t i;
 
     for (i = 0; i < facility_table_count; i++) {
         if (ntstts_facility_table[i].value == facility)
@@ -28,10 +28,10 @@ ntstts_lookup_facility(uint32_t facility)
     return NULL;
 }
 
-const struct *ntstts_status_entry
+const struct ntstts_status_entry *
 ntstts_lookup_status(uint32_t status)
 {
-    size_t = i;
+    size_t i;
 
     for (i = 0; i < status_table_count; i++) {
         if (ntstts_status_table[i].value == status)
